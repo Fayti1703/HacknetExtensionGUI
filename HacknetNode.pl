ac2fdcb0a9371b0df0af2f9699990da9e3f45d77
ryangeezy <subliminalproductionsrg@gmail.com>
2017-05-23 19:23:36 -0600

    17.5.23.0
    Having problems getting the @nodeAccounts array value to update after passing a reference of it to a subroutine that modifies the data. Once that's fixed it can be applied to finish building the rest of the UI.

--- HacknetNode.pl
+++ HacknetNode.pl
@@ -1,18 +1,51 @@
 #!/usr/bin/perl
 
 use Tk;
+use warnings;
+use strict;
+use diagnostics;
 require Tk::Pane;
+require Tk::HList;
+
+package main;
 my $mw = MainWindow->new();
-$mw->geometry( "750x1000" );
-$mw->optionAdd('*font' => 'fixed');
+	$mw->geometry( "1000x900" );
+	$mw->optionAdd('*font' => 'fixed');
 my $rows = 100;
+my $allowDBMBool = "false";
+my $trackerBool = 0;
+my $deathRowDatabaseBool = 0;
+my $academicDatabaseBool = 0;
+my $ispSystemBool = 0;
+my $medicalDatabaseBool = 0;
+my $newAccountUsernameText;
+my $newAccountUsernameEntry;
+my $nodeAccountPasswordText;
+my $nodeAccountPasswordEntry;
+my $nodeAccountTypeText;
+my $nodeAccountTypeEntry;
+my $newFirewallLevelText;
+my $newFirewallLevelEntry;
+my $nodeFirewallSolutionText;
+my $nodeFirewallSolutionEntry;
+my $nodeFirewallTimeText;
+my $nodeFirewallTimeEntry;
+my $newFilePathText;
+my $newFilePathEntry;
+my $nodeFileNameText;
+my $nodeFileNameEntry;
+my $nodeFileContentsText;
+my $nodeFileContentsEntry;
+my $nodeAccountsHList;
+
 my @r;
-my $AllowDBMBool = "false";
+my @nodeAccounts;
+
 
 for my $i(1..$rows){
 	push @r, $i;
 };
-
+################################################################################################################################################################
 my $frame = $mw->Scrolled(
     'Frame',
     -scrollbars => "e",
@@ -104,9 +137,9 @@ my $nodeAllowDBMCheckbuttonText = $frame->Label(
 	);
 
 my $nodeAllowDBMCheckbutton = $frame->Checkbutton(
-	-onvalue => "true",
-	-offvalue => "false",
-	-variable => $AllowDBMBool,
+	-offvalue =>"false",
+	-onvalue =>"true",
+	-variable => \$allowDBMBool,
 	)->grid(
 	-row=>$r[0],
 	-column=>2,
@@ -171,22 +204,7 @@ my $nodeAccountText = $frame->Label(
 
 my $nodeAccountNewButton = $frame->Button(
 	-text => 'New',
-	)->grid(
-	-row=>$r[0],
-	-column=>2,
-	-sticky=>'w',
-	);
-splice(@r,0,1);
-my $nodeMailAccountText = $frame->Label(
-	-text => 'Mail Account: ',
-	)->grid(
-	-row=>$r[0],
-	-column=>1,
-	-sticky=>"e",
-	);
-
-my $nodeMailAccountNewButton = $frame->Button(
-	-text => 'New',
+	-command=> sub { &newAccount() }
 	)->grid(
 	-row=>$r[0],
 	-column=>2,
@@ -256,6 +274,7 @@ my $nodeFirewallText = $frame->Label(
 
 my $nodeFirewallEntry = $frame->Button(
 	-text=>"New",
+	-command=>sub {&newFirewall()},
 	)->grid(
 	-row=>$r[0],
 	-column=>2,
@@ -304,6 +323,7 @@ my $nodeTrackerCheckbuttonText = $frame->Label(
 	);
 
 my $nodeTrackerCheckbutton = $frame->Checkbutton(
+	-variable => \$trackerBool,
 	)->grid(
 	-row=>$r[0],
 	-column=>2,
@@ -318,9 +338,8 @@ my $nodeDLinkText = $frame->Label(
 	-sticky=>"e",
 	);
 
-my $nodeDLinkEntry = $frame->Entry(
-	-width => 20,
-	-textvariable => "advExamplePC2",
+my $nodeDLinkEntry = $frame->Button(
+	-text => "New",
 	)->grid(
 	-row=>$r[0],
 	-column=>2,
@@ -353,6 +372,7 @@ my $nodeFileText = $frame->Label(
 
 my $nodeFileNewButton = $frame->Button(
 	-text => 'New',
+	-command => sub{&newFile()}
 	)->grid(
 	-row=>$r[0],
 	-column=>2,
@@ -464,6 +484,7 @@ my $nodeDeathRowDatabaseText = $frame->Label(
 	);
 
 my $nodeDeathRowDatabaseCheckbutton = $frame->Checkbutton(
+	-variable => \$deathRowDatabaseBool,
 	)->grid(
 	-row=>$r[0],
 	-column=>2,
@@ -479,6 +500,7 @@ my $nodeAcademicDatabaseText = $frame->Label(
 	);
 
 my $nodeAcademicDatabaseCheckbutton = $frame->Checkbutton(
+	-variable => \$academicDatabaseBool,
 	)->grid(
 	-row=>$r[0],
 	-column=>2,
@@ -494,6 +516,7 @@ my $nodeMedicalDatabaseText = $frame->Label(
 	);
 
 my $nodeMedicalDatabaseCheckbutton = $frame->Checkbutton(
+	-variable => \$medicalDatabaseBool,
 	)->grid(
 	-row=>$r[0],
 	-column=>2,
@@ -509,6 +532,7 @@ my $nodeISPSystemText = $frame->Label(
 	);
 
 my $nodeISPSystemCheckbutton = $frame->Checkbutton(
+	-variable => \$ispSystemBool,
 	)->grid(
 	-row=>$r[0],
 	-column=>2,
@@ -784,10 +808,12 @@ my $nodeIRCDaemonNewButton = $frame->Button(
 	-column=>2,
 	-sticky=>'w',
 	);
+################################################################################################################################################################
+################################################################################################################################################################
 splice(@r,0,1);
 my $generateButton = $frame->Button(
     -text    => 'Generate XML',
-    -command => sub { genXML() },
+    -command => sub { &genXML() },
     )->grid(
 	-row=>$r[0],
 	-column=>1,
@@ -802,9 +828,260 @@ my $quitButton = $frame->Button(
 	-column=>1,
 	-columnspan=>3,
 	);
+################################################################################################################################################################
+################################################################################################################################################################
+sub createAddOKButtons(){
+	my $window = $_[0];
+	my @nodeList = @{$_[1]};
+	my $nodeHList = $_[2];
+	my $addButton = $window->Button(
+	    -text => 'Add',
+	    -command => sub {&addPressed($window, \@nodeList, $nodeHList)},
+	    )->grid(
+		-row=>4,
+		-column=>1,
+		-columnspan=>2,
+		);
+	my $okButton = $window->Button(
+	    -text => 'OK',
+	    -command => sub {@{$_[1]} = &okPressed($window, \@nodeList)},
+	    )->grid(
+		-row=>5,
+		-column=>1,
+		-columnspan=>2,
+		);
+}
+sub addPressed(){
+	my $window = $_[0];
+	my @nodeList = @{$_[1]};
+	my $nodeHList = $_[2];
+	&addRecord($window, \@nodeList, $nodeHList);
+}
+sub okPressed(){
+	my $window = $_[0];
+	my @nodeList = @{$_[1]};
+	$window->destroy;
+	return @nodeList;
+}
+sub addRecord{
+	my $window = $_[0];
+	my @list = @{$_[1]};
+	my $listLength = scalar(@list)/3;
+	my $nodeHList = $_[2];
+	my @children = $window->children;
+	foreach my $widget (@children) {
+		if($widget->isa('Tk::Entry')){
+			push @list, $widget->get;
+		}
+	}
+
+	$nodeHList->add($list[$listLength]);
+	$nodeHList->itemCreate($list[$listLength], 0, -text=>$list[0]);
+	$nodeHList->itemCreate($list[$listLength], 1, -text=>$list[1]);
+	$nodeHList->itemCreate($list[$listLength], 2, -text=>$list[2]);
+}
+
+sub showRecords{
+	my $window = $_[0];
+	my @list = @{$_[1]};
+	my $listLength = scalar(@list)/3;
+	my $HList = $_[2];
+	print @{$_[1]}."\n";
+	#foreach my $node (@nodeList){
+	#	$nodeHList->add($node);
+	#	$nodeHList->itemCreate($node, 0, -text=>$node);
+	#}
+}
+
+sub newAccount{
+	my $newAccountWindow = MainWindow->new(-title=>"New Account");
+	$newAccountWindow->geometry( "1000x300+500+300" );
+	$newAccountWindow->focusForce;
+	$newAccountUsernameText = $newAccountWindow->Label(
+		-text => 'Username: ',
+		)->grid(
+		-row=>1,
+		-column=>1,
+		-sticky=>"e",
+		);
+
+	$newAccountUsernameEntry = $newAccountWindow->Entry(
+		-width => 20,
+		)->grid(
+		-row=>1,
+		-column=>2,
+		-sticky=>'w',
+		);
+	$nodeAccountPasswordText = $newAccountWindow->Label(
+		-text => 'Password: ',
+		)->grid(
+		-row=>2,
+		-column=>1,
+		-sticky=>"e",
+		);
+
+	$nodeAccountPasswordEntry = $newAccountWindow->Entry(
+		-width => 20,
+		)->grid(
+		-row=>2,
+		-column=>2,
+		-sticky=>'w',
+		);
+	$nodeAccountTypeText = $newAccountWindow->Label(
+		-text => 'Account Type: ',
+		)->grid(
+		-row=>3,
+		-column=>1,
+		-sticky=>"e",
+		);
+
+	$nodeAccountTypeEntry = $newAccountWindow->Entry(
+		-width => 20,
+		)->grid(
+		-row=>3,
+		-column=>2,
+		-sticky=>'w',
+		);
+
+	$nodeAccountsHList = $newAccountWindow->HList(
+		-columns => 4,
+		-header => -1,
+		-width => 100,
+		-command => sub{&deleteRecord($nodeAccountsHList)},
+		)->grid(
+		-row=>1,
+		-column=>3,
+		-rowspan=>3,
+		-sticky=>'w',
+		);
+
+	$nodeAccountsHList->headerCreate(0, -text => "Username",);
+	$nodeAccountsHList->headerCreate(1, -text => "Password",);
+	$nodeAccountsHList->headerCreate(2, -text => "Type",);
+
+	&createAddOKButtons($newAccountWindow, \@nodeAccounts, $nodeAccountsHList);
+
+	&showRecords($newAccountWindow, \@nodeAccounts, $nodeAccountsHList);
+
+}
+
+sub deleteRecord{
+	my $nodeHList = $_[0];
+	my $path;
+	$path=$nodeHList->selectionGet;
+	$nodeHList->delete('entry', $path);
+	##NEEDS TO DELETE FROM ARRAY
+}
+
+sub newFirewall{
+	my $newFirewallWindow = MainWindow->new(-title=>"New Firewall");
+	$newFirewallWindow->geometry( "400x200+500+400" );
+	$newFirewallWindow->focusForce;
+	$newFirewallLevelText = $newFirewallWindow->Label(
+		-text => 'Level: ',
+		)->grid(
+		-row=>1,
+		-column=>1,
+		-sticky=>"e",
+		);
+
+	$newFirewallLevelEntry = $newFirewallWindow->Spinbox(
+		-width=>3,
+		-from=>0,
+		-to=>99,
+		)->grid(
+		-row=>1,
+		-column=>2,
+		-sticky=>'w',
+		);
+	$nodeFirewallSolutionText = $newFirewallWindow->Label(
+		-text => 'Solution: ',
+		)->grid(
+		-row=>2,
+		-column=>1,
+		-sticky=>"e",
+		);
+
+	$nodeFirewallSolutionEntry = $newFirewallWindow->Entry(
+		-width => 20,
+		)->grid(
+		-row=>2,
+		-column=>2,
+		-sticky=>'w',
+		);
+	$nodeFirewallTimeText = $newFirewallWindow->Label(
+		-text => 'Additional Time: ',
+		)->grid(
+		-row=>3,
+		-column=>1,
+		-sticky=>"e",
+		);
+
+	$nodeFirewallTimeEntry = $newFirewallWindow->Entry(
+		-width => 20,
+		)->grid(
+		-row=>3,
+		-column=>2,
+		-sticky=>'w',
+		);
+
+	#&createAddOKButtons($newFirewallWindow);
+}
+
+sub newFile{
+	my $newFileWindow = MainWindow->new(-title=>"New File");
+	$newFileWindow->geometry( "400x200+500+450" );
+	$newFileWindow->focusForce;
+	$newFilePathText = $newFileWindow->Label(
+		-text => 'Path: ',
+		)->grid(
+		-row=>1,
+		-column=>1,
+		-sticky=>"e",
+		);
+
+	$newFilePathEntry = $newFileWindow->Entry(
+		-width=>50,
+		)->grid(
+		-row=>1,
+		-column=>2,
+		-sticky=>'w',
+		);
+	$nodeFileNameText = $newFileWindow->Label(
+		-text => 'Name: ',
+		)->grid(
+		-row=>2,
+		-column=>1,
+		-sticky=>"e",
+		);
+
+	$nodeFileNameEntry = $newFileWindow->Entry(
+		-width => 50,
+		)->grid(
+		-row=>2,
+		-column=>2,
+		-sticky=>'w',
+		);
+	$nodeFileContentsText = $newFileWindow->Label(
+		-text => 'Contents: ',
+		)->grid(
+		-row=>3,
+		-column=>1,
+		-sticky=>"e",
+		);
+
+	$nodeFileContentsEntry = $newFileWindow->Entry(
+		-width => 50,
+		)->grid(
+		-row=>3,
+		-column=>2,
+		-sticky=>'w',
+		);
+	#&createAddOKButtons($newFileWindow);
+}
 
 MainLoop;
-
+################################################################################################################################################################
 sub genXML{
 	my $name = $nodeNameEntry->get;
 	my $fileName = join "", $name, ".xml";
@@ -815,17 +1092,74 @@ sub genXML{
 	print OUTFILE '" name="'.$nodeNameEntry->get;
 	print OUTFILE '" ip="'.$nodeIPEntry->get;
 	print OUTFILE '" security="'.$nodeSecurityEntry->get;
-	print OUTFILE '" allowsDefaultBootModule="'.$AllowDBMBool;
+	print OUTFILE '" allowsDefaultBootModule="'.$allowDBMBool;
 	print OUTFILE '" icon="'.$nodeIconEntry->get;
 	print OUTFILE '" type="'.$nodeTypeEntry->get.'" >'."\n";
 
- 	print OUTFILE "\t".'<adminPass pass='.'"'.$nodePWEntry->get.'" />'."\n";
+ 	print OUTFILE "\t".'<adminPass pass="'.$nodePWEntry->get.'" />'."\n";
 
  	print OUTFILE "\t".'<ports>'.$nodePortsEntry->get.'</ports>'."\n";
 
- 	print OUTFILE "\t".'<portsForCrack val='.'"'.$nodePortsNeededEntry->get.'" />'."\n";
+ 	print OUTFILE "\t".'<proxy time="'.$nodeProxyTimeEntry->get.'" />'."\n";
 
- 	print OUTFILE "\t".'<trace time ='.'"'.$nodeTraceTimeEntry->get.'" />'."\n";
+ 	print OUTFILE "\t".'<portsForCrack val="'.$nodePortsNeededEntry->get.'" />'."\n";
+
+ 	print OUTFILE "\t".'<trace time ="'.$nodeTraceTimeEntry->get.'" />'."\n";
+
+ 	print OUTFILE "\t".'<portRemap>'.$nodePortRemapEntry->get.'</portRemap>'."\n";
+
+ 	#for (my $i = 0; $i < scalar(@nodeAccounts); $i += 3) {
+ 		print OUTFILE "\t".'<account username="'.$nodeAccounts[0].'" password="'.$nodeAccounts[1].'" type="'.$nodeAccounts[2].'" />'."\n";
+	#}
+
+ 	#if(trackerBool){print OUTFILE "\t".'<tracker />'."\n";}else{print OUTFILE "\t".'<!--<tracker />-->'."\n";}
+
+ 	#if(deathRowDatabaseBool){print OUTFILE "\t".'<deathRowDatabase />'."\n";}else{print OUTFILE "\t".'<!--<deathRowDatabase />-->'."\n";}
+
+ 	#if(academicDatabaseBool){print OUTFILE "\t".'<academicDatabase />'."\n";}else{print OUTFILE "\t".'<!--<academicDatabase />-->'."\n";}
+
+ 	#if(ispSystemBool){print OUTFILE "\t".'<ispSystem />'."\n";}else{print OUTFILE "\t".'<!--<ispSystem />-->'."\n";}
+
+	#if(medicalDatabaseBool){print OUTFILE "\t".'<medicalDatabase />'."\n";}else{print OUTFILE "\t".'<!--<medicalDatabase />-->'."\n";}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  	print OUTFILE '</Computer>';
 
